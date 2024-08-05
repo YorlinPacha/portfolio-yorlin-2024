975a0a1daf82e82882274922a3f450951b7c5844
Yorlin Pachajoa <yorlin1517@gmail.com>
2024-08-05 22:47:07 +0200

    Implementacion de los demos

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
 
-function ProjectCard({src, link, h3, p, tecnologias}) {
+function ProjectCard({ src, link, h3, p, tecnologias, mostrarDemo, linkDemo }) {
   return (
-    <a href={link} target="_blank" className='enlaceProyectos'>
-          <img
-            className="hover"
-            src={src}
-            alt={`Imagen de ${h3}`}
-          />
-          <h3>{h3}</h3>
-          <p>{p}</p>
-          <p className='pTecnologias'>{tecnologias}</p>
-        </a>
-  )
+    <div className="contenedorEnlace">
+      <a href={link} target="_blank" className="enlaceProyectos">
+        <img className="hover" src={src} alt={`Imagen de ${h3}`} />
+        <h3>{h3}</h3>
+        <p>{p}</p>
+        <p className="pTecnologias">{tecnologias}</p>
+      </a>
+      <br />
+      {/* Aplicar la clase oculto si mostrarDemo es falso */}
+      <a
+        href={linkDemo}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`demo ${!mostrarDemo ? "oculto" : ""}`}
+      >
+        Demo
+      </a>
+    </div>
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -30,19 +30,25 @@ function Projects() {
             link={"https://github.com/YorlinPacha/ProyectoFinal-SestaoNow"}
             h3={"Sestao Now"}
             p={"Portal de anuncios"}
-            tecnologias={"Vue3 - JavaScript - SQL - Bootstrap - Python"}/>
+            tecnologias={"Vue3 - JavaScript - SQL - Bootstrap - Python"}
+            mostrarDemo={"true"}
+            linkDemo={"https://www.youtube.com/watch?v=SdVYx2X-Ec4"}/>
         <ProjectCard
             src={imgSpace}
             link={"https://github.com/YorlinPacha/Mi-Space-ToDo-List"}
             h3={"Space To Do"}
             p={"Lista de Tareas"}
-            tecnologias={"JavaScript - Vue3 - HTML - CSS3"}/>
+            tecnologias={"JavaScript - Vue3 - HTML - CSS3"}
+            mostrarDemo={"true"}
+            linkDemo={"https://space-todo.netlify.app/"}/>
         <ProjectCard
             src={weatherImg}
             link={"https://github.com/YorlinPacha/app_weather"}
             h3={"Weather F5"}
             p={"Aplicacion del clima"}
-            tecnologias={"Html - CSS - Vue3 - JavaScript - Bootstrap"}/>
+            tecnologias={"Html - CSS - Vue3 - JavaScript - Bootstrap"}
+            mostrarDemo={"true"}
+            linkDemo={"https://myweatherf5.netlify.app/"}/>
       </div>
     </section>
   );
